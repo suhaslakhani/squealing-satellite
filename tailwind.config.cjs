44a65a992850020f3aed4997b45192b98acd1510
Suhas Lakhani <lakhanisuhas10@gmail.com>
2023-08-11 16:10:22 +0530

    screen and spacing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,20 @@ module.exports = {
 	theme: {
 		extend: {
 			colors:{
+				screens: {
+					sm: '480px',
+					md: '768px',
+					lg: '976px',
+					xl: '1440px',
+				},
+				spacing: {
+					'1': '200px',
+					'2': '12px',
+					'3': '16px',
+					'4': '24px',
+					'5': '32px',
+					'6': '48px',
+				},
 				vuejs: {
 					100:"#3f6212",
 					110: "#122334"
