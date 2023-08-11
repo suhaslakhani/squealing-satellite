98b91017bdd224f53293fbc8e04064e793bf8070
Suhas Lakhani <lakhanisuhas10@gmail.com>
2023-08-11 15:50:04 +0530

    color-customization

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				vuejs: {
+					100:"#3f6212",
+					110: "#122334"
+				}
+			},
+		},
 	},
 	plugins: [],
 }
